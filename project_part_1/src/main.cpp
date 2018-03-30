0383cc1a96482a1085051f0d339828492c384f35
Duarte Carvalho <up201503661@fe.up.pt>
2018-03-30 13:02:14 +0100

    added files to interact with graphviewer

--- project_part_1/src/main.cpp
+++ project_part_1/src/main.cpp
@@ -2,10 +2,13 @@
 // Created by dnc on 30-03-2018.
 //
 #include <iostream>
+#include "graphviewer.h"
 
 int main (int argc, char* argv[]) {
 
-    std::cout << "Hello World!\n" ;
+    GraphViewer *gv = new GraphViewer(600, 600, true);
+
+    gv->createWindow(600, 600);
 
     return 0;
 }
