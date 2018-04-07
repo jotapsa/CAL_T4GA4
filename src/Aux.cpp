5d61c63bfa93e3844e4ebc17db384371c8aa08d5
Duarte Carvalho <up201503661@fe.up.pt>
2018-04-07 15:29:59 +0100

    node map representation finally good

--- src/Aux.cpp
+++ src/Aux.cpp
@@ -64,35 +64,9 @@ double getDistance(double lat1, double lon1, double lat2, double lon2) {
 }
 
 std::pair<int, int> convertToCoords(double dLat, double dLon, double rLon, double rLat) {
-/*
-    double distToOriginX = (dLon - MIN_LONGITUDE);
-    double distToOriginy = (dLat - MIN_LATITUDE);
-    auto x = (int)(distToOriginX * windowWidth / std::abs(DELTA_LONGITUDE));
-    auto y = (int)(distToOriginy * windowHeight / std::abs(DELTA_LATITUDE));
 
+    int x = (dLon - MIN_LONGITUDE) * IMAGE_X / DELTA_X;
+    int y = IMAGE_Y - (dLat - MIN_LATITUDE) * IMAGE_Y / DELTA_Y;
 
-
-    static double minY = log(tan((90 + MIN_LATITUDE) * PI / 360)) / (PI / 180);
-    static double maxX = MAX_LONGITUDE * 20037508.34 / 180;
-    static double maxY = log(tan((90 + MAX_LATITUDE) * PI / 360)) / (PI / 180);
-    static double deltaX = maxX - minX;
-    static double deltaY = maxY - minY;
-    double xx = dLon * 20037508.34 / 180;
-    double yy = log(tan((90 + dLat) * PI / 360)) / (PI / 180);
-
-    yy = yy * 20037508.34 / 180;
-
-    double distToOriginX = (xx - minX);
-    double distToOriginy = (yy - minY);
-    int x = (int)(distToOriginX * 1900 / deltaX);
-    int y = (int)(distToOriginy * 1900 / deltaY);
-
-    std::cout << "x= " << x << " y= " << y << std::endl;*/
-
-//TODO(Mercator conversion)
-
-    double x = (dLon - MIN_LONGITUDE) * IMAGE_X / DELTA_X;
-    double y = (dLat - MIN_LATITUDE) * IMAGE_Y / DELTA_Y;
-
-    return std::make_pair(400+ x,1620 -y);
+    return std::make_pair(x,y);
 }
